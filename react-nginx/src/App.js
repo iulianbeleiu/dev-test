0437f94a8062156963d6e0b23ca59264c1e298ef
beleiui <i.beleiu@plan-net.com>
2020-10-03 16:58:06 +0300

    create login form, check mongo connection

--- react-nginx/src/App.js
+++ react-nginx/src/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import LoginPage from './components/LoginPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to react!
-        </p>
-      </header>
-    </div>
+    <LoginPage />
   );
 }
 
---
+++ react-nginx/src/components/LoginPage.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import API from '../api';
+
+class LoginPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { email: '' };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange = event => {
+        this.setState({ email: event.target.value });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault()
+        API.get(`/mongo/works`)
+        .then(res => {
+            if (200 === res.status) {
+                console.log('Mongo works!')
+            }
+        })
+    }
+    render() {
+        return (
+            <div className="LoginPage">
+                <form onSubmit={this.handleSubmit}>
+                    <h3>Log In</h3>
+                    <div className="form-group">
+                        <label>Email address</label>
+                        <input
+                            type="email"
+                            name="email"
+                            className="form-control"
+                            placeholder="Enter email"
+                            value={this.state.value}
+                            onChange={this.handleChange}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Password</label>
+                        <input
+                            name="password"
+                            type="password"
+                            className="form-control"
+                            placeholder="Enter password"
+                        />
+                    </div>
+                    <button type="submit" className="btn btn-primary btn-block">Submit</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default LoginPage;
